5dd7b979747ac372c01ee0f200554bc18b0c7888
تيسير تيسير <tysyrt200@gmail.com>
2024-03-25 02:17:21 +0300

    Upload Is Done 3>

---
+++ main.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector(".scrole-to-top");
+
+
+window.onscroll = function () {
+    if (window.scrollY >= 600) {
+        btn.style.display = "block";
+    } else {
+        
+        btn.style.display = "none";
+    }
+}
+
+btn.onclick = function () {
+    window.scrollTo ({
+        top: 0,
+        behavior: "smooth"
+    })
+}
