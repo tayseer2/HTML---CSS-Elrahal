c315da63dd17ac8f74c2226b4c69ad4a8751946b
تيسير تيسير <tysyrt200@gmail.com>
2024-03-25 21:36:27 +0300

    Upload Is Done

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
+// Scroll To Top
 let btn = document.querySelector(".scrole-to-top");
-
-
 window.onscroll = function () {
     if (window.scrollY >= 600) {
         btn.style.display = "block";
     } else {
-        
+
         btn.style.display = "none";
     }
 }
-
 btn.onclick = function () {
-    window.scrollTo ({
+    window.scrollTo({
         top: 0,
         behavior: "smooth"
     })
