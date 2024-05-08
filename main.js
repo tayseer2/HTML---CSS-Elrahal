0766fcdcae4e7b3c32bde48929c7f59c450cac1f
تيسير تيسير <tysyrt200@gmail.com>
2024-05-08 16:24:41 +0300

    Upload Is Done

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ window.onscroll = function () {
         btn.style.display = "none";
     }
 }
+window.onload = function () {
+    btn.style.display = "none";
+}
 btn.onclick = function () {
     window.scrollTo({
         top: 0,
@@ -19,13 +22,37 @@ let popDiv = document.querySelector(".popup");
 let popBtn = document.querySelector(".btn");
 
 
-setTimeout(shwoPopUP, 5000)
-function shwoPopUP() {
-    popDiv.style.display = "block"
+// setTimeout(shwoPopUP, 5000)
+// function shwoPopUP() {
+//     popDiv.style.display = "block"
+// }
+
+// popBtn.onclick = function () {
+//     popDiv.remove()
+// }
+
+
+let days = document.querySelector(".days");
+let hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+
+let count = setInterval(counter, 1000)
+function counter() {
+    seconds.innerHTML -= 1
+    if (seconds.innerHTML < "0") {
+        minutes.innerHTML -= 1;
+        seconds.innerHTML = 59;
+    } 
+     if (minutes.innerHTML < "0") {
+        hours.innerHTML -= 1;
+        minutes.innerHTML = 59;
+    }
+    if (hours.innerHTML < "0") {
+        days.innerHTML -= 1;
+        hours.innerHTML = 23;
+    }
+    if (seconds.innerHTML === "0" && minutes.innerHTML === "0" && hours.innerHTML === "0" && days.innerHTML === "0") {
+        clearInterval(count)
+    }
 }
-
-popBtn.onclick = function () {
-    popDiv.remove()
-}
-
-
