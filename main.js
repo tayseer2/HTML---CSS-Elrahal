2005fa01c76eace8ee983dec7ce9b68f4e43f5ba
تيسير تيسير <tysyrt200@gmail.com>
2024-05-09 16:59:47 +0300

    OK

--- main.js
+++ main.js
@@ -17,11 +17,9 @@ btn.onclick = function () {
     })
 }
 
-
+// Popup 
 let popDiv = document.querySelector(".popup");
 let popBtn = document.querySelector(".btn");
-
-
 setTimeout(shwoPopUP, 5000)
 function shwoPopUP() {
     popDiv.style.display = "block"
@@ -31,12 +29,11 @@ popBtn.onclick = function () {
     popDiv.remove()
 }
 
-
+// counter   
 let days = document.querySelector(".days");
 let hours = document.querySelector(".hours");
 let minutes = document.querySelector(".minutes");
 let seconds = document.querySelector(".seconds");
-
 let count = setInterval(counter, 1000)
 function counter() {
     seconds.innerHTML -= 1
@@ -56,3 +53,19 @@ function counter() {
         clearInterval(count)
     }
 }
+
+// progress
+
+let prog1 = document.querySelector(".prog1");
+let prog2 = document.querySelector(".prog2");
+let prog3 = document.querySelector(".prog3");
+let prog4 = document.querySelector(".prog4");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 7350) {
+        prog1.style.width = "80%";
+        prog2.style.width = "85%";
+        prog3.style.width = "70%";
+        prog4.style.width = "65%";
+    }
+})
